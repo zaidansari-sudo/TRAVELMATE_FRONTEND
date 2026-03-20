b74c7327751a27740053f63e8e830fe8743c7147
zaidansari-sudo <zaidansari0515@gmail.com>
2026-03-20 14:53:48 +0530

    fix: disable prerender for thankyou page

--- src/components/Thankyou/Thankyou.jsx
+++ src/components/Thankyou/Thankyou.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { CheckCircle, Mail, Calendar, Download, ArrowRight, Home } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
